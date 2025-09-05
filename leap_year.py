2fe1acc03a0e9006cd0216691b795422cf20fd51
Introducción a la Programación 1 y 2 <mbrunettaalvarezper@mail.austral.edu.ar>
2025-09-05 11:03:46 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,6 @@
 def leap_year():
     anio = int(input("Ingrese un año: "))
     if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0):
-        print(f"El año {anio} es bisiseto")
+        print(f"El año {anio} es bisiesto")
     else:
         print(f"El año {anio} no es bisiesto")
