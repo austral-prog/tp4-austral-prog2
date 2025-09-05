8ea40e578a5dbb36cd6c530dc1c1252160588c12
Introducción a la Programación 1 y 2 <mbrunettaalvarezper@mail.austral.edu.ar>
2025-09-05 10:54:18 -0300

    Update line.py

--- line.py
+++ line.py
@@ -7,14 +7,14 @@ def line():
     print(f"El coeficiente B de su ecuación de la recta es: {b}")
     print(f"El coeficiente X1 de su ecuación de la recta es: {x1}")
     print(f"El coeficiente X2 de su ecuación de la recta es: {x2}")
-    print("\n")
+    print("")
     print("Para la siguiente ecuación:")
     print(f"\tY = {a}X + {b}")
-    print("\n")
+    print("")
     print("Dados los siguientes puntos:")
     y1 = a*x1+b
     y2 = a*x2+b
     print(f"\tP1 ({x1}, {y1})")
     print(f"\tP2 ({x2}, {y2})")
-    print("\n")
+    print("")
     print(f"La distancia entre ellos es: {((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5}")
